b1ade29c365add0cd9944f99f10268da97b69ab0
Poushi <afrinsultana@iut-dhaka.edu>
2023-05-30 16:39:32 +0600

    completed Lesson 1 - Drum Kit #1

---
+++ L1_Drum-Kit/drumKit.js
@@ -0,0 +1,31 @@
+// function for playing audio
+function playSound(e) {
+  // console.log(e);
+  const dataKey = `data-key="${e.keyCode}"`; //getting the ascii code from keyboard
+  audio = document.querySelector(`audio[${dataKey}]`);
+  // console.log(audio);
+
+  if (!audio) return; // stop playing all audio together
+  audio.currentTime = 0; //rewind the audio from start
+  audio.play();
+
+  // add listenerevent for keys
+  const key = document.querySelector(`.key[${dataKey}]`);
+  // console.log(key);
+  // add effects on pressing key
+  if (key) key.classList.add("playing");
+}
+function removeEffect(e) {
+  // console.log(e.propertyName);
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("playing");
+}
+
+// remove effects on each keys
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => {
+  key.addEventListener("transitionend", removeEffect);
+});
+
+// add audio on keydown
+window.addEventListener("keydown", playSound);
